130e25a3f90d3dc30a63c682a086a490ee21acd3
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2021-12-15 21:39:10 +0200

    Create EssentialFeed framework along with a FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,13 @@
+//
+//  Created by Vitalii on 15.12.2021.
+//
+
+import Foundation
+
+struct FeedItem {
+    
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
