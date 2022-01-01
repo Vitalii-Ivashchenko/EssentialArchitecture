ae997781601268b06876a2b4cd3863960a42c110
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-01-01 16:04:31 +0200

    Retrieving from empty cache delivers empty result

---
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  Created by Vitalii on 01.01.2022.
+//
+
+import EssentialFeed
+import XCTest
+
+class CodableFeedStore {
+    
+    func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
+        completion(.empty)
+    }
+}
+
+class CodableFeedStoreTests: XCTestCase {
+
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = CodableFeedStore()
+        let expectation = self.expectation(description: "Wait for cache retrieve")
+        
+        sut.retrieve { result in
+            switch result {
+            case .empty:
+                break
+            default:
+                XCTFail("Expected empty result, got \(result) instead")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+}
