272e61c5357422696dfa81e4e50f8f600d1e2831
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-01-04 23:33:06 +0200

    Core Data Feed Store implementation

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -27,7 +27,7 @@ public final class RemoteFeedLoader: FeedLoader {
             
             switch result {
             case let .success(data, response):
-                completion(RemoteFeedLoader.map(data, from: response ))
+                completion(RemoteFeedLoader.map(data, from: response))
             case .failure:
                 completion(.failure(Error.connectivity))
             }
