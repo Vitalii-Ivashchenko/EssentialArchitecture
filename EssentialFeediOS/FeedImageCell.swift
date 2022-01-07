082a98d60540eaaff3ac69077374abbe29d2e8c3
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-01-07 22:47:18 +0200

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Vitalii on 07.01.2022.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
