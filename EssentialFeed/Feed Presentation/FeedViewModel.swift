a3b62f8f11b2e83b032819c9166e86d997c7fbd3
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-01-13 01:57:54 +0200

    Move `FeedViewModel` to separate file

---
+++ EssentialFeed/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Vitalii on 13.01.2022.
+//
+
+import Foundation
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
