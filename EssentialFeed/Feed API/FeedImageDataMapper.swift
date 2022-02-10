42b97ae7b0f95fb20069ff46957f05cda0961968
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-02-11 00:00:25 +0200

    Test `FeedImageDataMapper` in isolation

---
+++ EssentialFeed/Feed API/FeedImageDataMapper.swift
@@ -0,0 +1,23 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Vitalii on 10.02.2022.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+    
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+        
+        return data
+    }
+}
