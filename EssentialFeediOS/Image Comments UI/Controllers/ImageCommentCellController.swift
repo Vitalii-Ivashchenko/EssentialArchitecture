f9dd5531022f209311d7e5a48502bbb190f70018
Vitalii Ivashchenko <vi.algorithm@gmail.com>
2022-11-20 12:14:29 +0200

    Add default implementation for optional methods in the `CellController` protocol

--- EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
+++ EssentialFeediOS/Image Comments UI/Controllers/ImageCommentCellController.swift
@@ -21,12 +21,4 @@ public class ImageCommentCellController: CellController {
         
         return cell
     }
-    
-    public func preload() {
-        
-    }
-    
-    public func cancelLoad() {
-        
-    }
 }
